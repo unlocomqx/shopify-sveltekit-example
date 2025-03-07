67d9422595f162dae368aa0b0242cacde9b79fa0
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-03-07 15:22:02 +0100

    add readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Shopify SvelteKit Example
+
--- shopify-kit/vite.config.ts
+++ shopify-kit/vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-console.log(process.env.PORT);
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
